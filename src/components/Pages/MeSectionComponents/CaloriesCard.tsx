b807700bbb61c90622a3511b8d895ece8139d60f
Jota-ato <julioc-01@outlook.com>
2025-12-29 21:32:29 -0600

    fix: fix error with colors in me component

--- src/components/Pages/MeSectionComponents/CaloriesCard.tsx
+++ src/components/Pages/MeSectionComponents/CaloriesCard.tsx
@@ -1,7 +1,7 @@
 
 export default function CaloriesCard({ name, calories }: { name: string, calories: number }) {
     return (
-        <div className="col-span-2 bg-gray-200 rounded-xl p-4 shadow border border-gray-300 dark:border-gray-600 space-y-4">
+        <div className="col-span-2 bg-surface-gray rounded-xl p-4 border border-surface-dark space-y-4">
             <h2 className="md:text-3xl text-2xl font-bold">{name}</h2>
             <p className="text-2xl font-bold"><span className="font-black text-primary">{Math.round(calories)}</span>{' '}
                 Calorías al día</p>
--- src/components/Pages/MeSectionComponents/DataCard.tsx
+++ src/components/Pages/MeSectionComponents/DataCard.tsx
@@ -12,7 +12,7 @@ type DataCardProps = {
 export default function DataCard({ title, value, unit, tailwindStyles }: DataCardProps) {
 
     return (
-        <div className={`bg-gray-200 rounded-xl p-4 shadow border border-gray-300 dark:border-gray-600 space-y-4 ${tailwindStyles}`} >
+        <div className={`bg-surface-gray rounded-xl p-4 border border-surface-dark space-y-4 ${tailwindStyles}`} >
             <h3 className="text-3xl font-bold">{title}</h3>
             {typeof value === "number" ? <p className="text-2xl font-bold">{value}{unit}</p> : (value === "Male" || value === "Female") ? <p className="text-2xl font-bold">{translateGender(value)}</p> : (value === "Gain Muscle" || value === "Maintain Muscle" || value === "Lose Fat") ? <p className="text-2xl font-bold">{translatePurpose(value as PurposeType)}</p> : <p className="text-2xl font-bold">{translateGoal(value as GoalType)}</p>}
         </div>

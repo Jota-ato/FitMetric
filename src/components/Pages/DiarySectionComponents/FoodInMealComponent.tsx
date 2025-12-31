3bbba671b09d75f702db3d80c62b5138dcd8efc6
Jota-ato <julioc-01@outlook.com>
2025-12-31 14:53:59 -0600

    feat: refactor: add dark and light support to redesign of macrosGrid component

--- src/components/Pages/DiarySectionComponents/FoodInMealComponent.tsx
+++ src/components/Pages/DiarySectionComponents/FoodInMealComponent.tsx
@@ -13,13 +13,13 @@ export default function FoodInMealComponent({ food }: FoodInMealComponentProps) 
     return (
         <article className="w-full max-w-220 mx-auto p-4 border border-surface-gray-dark rounded-xl space-y-4">
             <div className="flex justify-between border-b pb-4 border-surface-gray-dark">
-                <div className="text-2xl md:text-3xl text-center">
+                <div className="text-xl md:text-2xl">
                     <p className="font-bold">{food.description}</p>
                     <p className="text-secondary font-light">{food.portionSize}{food.portionUnit}</p>
                 </div>
                 <div>
-                    <p className="text-2xl text-muted">Calorías</p>
-                    <p className="text-3xl text-muted">{calories}</p>
+                    <p className="text-xl text-muted">Calorías</p>
+                    <p className="text-2xl text-muted">{calories}</p>
                 </div>
             </div>
             <div className="flex justify-evenly py-4 text-muted text-2xl">
--- src/components/Pages/DiarySectionComponents/MacrosGrid.tsx
+++ src/components/Pages/DiarySectionComponents/MacrosGrid.tsx
@@ -8,14 +8,22 @@ interface MacrosGridProps {
 
 export default function MacrosGrid({ calories, protein, carbs, fats }: MacrosGridProps) {
     return (
-        <div className="flex flex-col gap-4 my-4">
-            <h3 className="text-center text-xl md:text-2xl text-muted">Macronutrientes</h3>
-            <div className="grid grid-cols-2 md:grid-cols-4 justify-center gap-4 p-8">
-                <p className="text-xl md:text-3xl">Calorías: <span className="font-bold">{calories}</span></p>
-                <p className="text-xl md:text-3xl">Proteínas: <span className="font-bold">{protein}g</span></p>
-                <p className="text-xl md:text-3xl">Carbohidratos: <span className="font-bold">{carbs}g</span></p>
-                <p className="text-xl md:text-3xl">Grasas: <span className="font-bold">{fats}g</span></p>
-            </div>
-        </div>
+        <article className="flex flex-col gap-4 my-4">
+            <header className="text-center text-xl md:text-2xl text-muted">Macronutrientes</header>
+            <section className="grid grid-cols-2 md:grid-cols-4 justify-center gap-4 p-8">
+                <div className="border flex justify-center items-center rounded-xl border-muted">
+                    <p className="text-xl md:text-2xl py-4 text-muted">Calorías: <span className="font-bold">{calories}</span></p>
+                </div>
+                <div className="border flex justify-center items-center rounded-xl border-red-500 dark:border-red-200">
+                    <p className="text-xl md:text-2xl py-4 text-red-500 dark:text-red-200">Proteínas: <span className="font-bold">{protein}g</span></p>
+                </div>
+                <div className="border flex justify-center items-center rounded-xl border-orange-500 dark:border-orange-200">
+                    <p className="text-xl md:text-2xl py-4 text-orange-500 dark:text-orange-200">Carbohidratos: <span className="font-bold">{carbs}g</span></p>
+                </div>
+                <div className="border flex justify-center items-center rounded-xl border-yellow-500 dark:border-yellow-200">
+                    <p className="text-xl md:text-2xl py-4 text-yellow-500 dark:text-yellow-200">Grasas: <span className="font-bold">{fats}g</span></p>
+                </div>
+            </section>
+        </article>
     )
 }
